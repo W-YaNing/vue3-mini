d688bd976a0741437b97cc5f5c1e99b54e567d7e
yaning <13011162787@qq.com>
2022-05-03 19:03:55 +0800

    添加了 computed

---
+++ src/reactivity/computed.ts
@@ -0,0 +1,29 @@
+
+
+import { ReactiveEffect } from './effect'
+
+class computedRefImpl {
+  private _getter: any
+  private _dirty: Boolean = true
+  private _value: any
+  private _effect: any
+  constructor(getter) {
+    this._getter = getter
+    this._effect = new ReactiveEffect(getter, () => {
+      if(!this._dirty) {
+        this._dirty = true
+      }
+    })
+  }
+  get value() {
+    if(this._dirty){
+      this._dirty = false
+      this._value = this._effect.run()
+    }
+    return this._value
+  }
+}
+
+export function computed(getter) {
+  return new computedRefImpl(getter)
+} 
