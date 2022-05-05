b07a735220f2ea498c9f17bdd796d8531e3d29c5
yaning <13011162787@qq.com>
2022-05-05 11:44:17 +0800

    实现element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -4,7 +4,13 @@ import { h } from '../../lib/guide-mini-vue.esm.js'
 export const App = {
   // not have template
   render() {
-    return h("div", "hi" + this.msg)
+    return h("div", {
+      id: "root",
+      class: ['red', 'hard']
+    }, [
+      h('p', { class: 'red' }, 'hi'),
+      h('p', { class: 'bule' }, 'mini-vue')
+    ])
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,5 @@
 import { createComponentInstance, setupComponent } from './component'
-
+import { isObject } from '../shared/index'
 export function render(vnode, container) {
   // patch
   patch(vnode, container)
@@ -8,11 +8,17 @@ export function render(vnode, container) {
 
 function patch(vnode, container) {
   // 处理组件 
-
   // 判断是不是element 类型
   // is element type 需要用element处理
-  // processElement(vnode, container)
-  processComponent(vnode, container)
+  if(typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if(isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container)
 }
 
 
@@ -26,6 +32,37 @@ function mountComponent(vnode, container) {
   setupComponent(instance)
   
   setupRenderEffect(instance, container)
+}
+
+// 处理element
+function mountElement(vnode, container) {
+  // tag
+  const el = document.createElement(vnode.type)
+
+  const { children, props } = vnode
+
+  // child
+  if(typeof children === 'string') {
+    el.textContent = children
+  } else {
+    mountChildren(vnode, el)
+  }
+  
+  // props
+  for(const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+
+  // append
+  container.append(el)
+  
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach(element => {
+    patch(element, container)
+  });
 }
 
 function setupRenderEffect(instance, container) {
